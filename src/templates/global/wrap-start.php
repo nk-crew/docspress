a8b9964646c52917fb4df28491ee98adbfe515af
Nikita <nkdevinfo@gmail.com>
2018-09-08 16:51:46 +0300

    added helper styles for default wp themes

--- src/templates/global/wrap-start.php
+++ src/templates/global/wrap-start.php
@@ -15,6 +15,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 get_header( 'docs' );
 
+$theme_class = '';
+
+// additional class for default theme to add fix styles.
+$current_theme = get_template();
+if ( in_array( $current_theme, array( 'twentyseventeen', 'twentysixteen', 'twentyfifteen' ), true ) ) {
+    $theme_class = ' docspress_theme_' . $current_theme;
+}
+
 ?>
-<div id="primary" class="content-area">
+<div id="primary" class="content-area<?php echo esc_attr( $theme_class ); ?>">
     <main id="main" class="site-main" role="main">
