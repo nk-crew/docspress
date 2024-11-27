61d835a6fda2833e96441b20585756fb3be15dab
Nikita <nkdevinfo@gmail.com>
2024-11-27 23:13:55 +0300

    fixed warning from comments output in block themes

--- src/templates/single/comments.php
+++ src/templates/single/comments.php
@@ -15,5 +15,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 // If comments are open or we have at least one comment, load up the comment template.
 if ( comments_open() || get_comments_number() ) {
+    // Prevent deprecated warning.
+    // Thanks to https://github.com/WordPress/gutenberg/blob/d9eb6d9a6d64650f08ee68792b88f914de62cda1/packages/block-library/src/comments/index.php#L53-L60 .
+    add_filter( 'deprecated_file_trigger_error', '__return_false' );
     comments_template();
+    remove_filter( 'deprecated_file_trigger_error', '__return_false' );
 }
